95e1b0f746f9efb2b4311e6d29061f88f6398939
Hugo Esteves <hugo.veigaa@gmail.com>
2024-01-15 17:53:16 +0000

    First version of the project:
    - create folder structure;
    - define Form component and create Todo;
    - add Routes to App.js

---
+++ src/components/Form/TodoCreationForm.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import axios from "axios";
+
+const TodoCreationForm = () => {
+  const [description, setDescription] = useState("");
+
+  const handleDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:5000/todos", {
+        description,
+      });
+
+      console.log(response.data[0]);
+      setDescription("");
+      // update list of todos
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="description"
+          placeholder="Write new task here..."
+          onChange={handleDescription}
+        />
+        <button type="submit">Create</button>
+      </form>
+    </section>
+  );
+};
+
+export default TodoCreationForm;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TodoCreationForm from "../../components/Form/TodoCreationForm";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello user</h1>
+      <TodoCreationForm />
+      <section>Card</section>
+    </div>
+  );
+};
+
+export default Home;
